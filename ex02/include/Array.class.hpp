88e7138e4bdc3aa857102fbb7e1df39631d75615
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2023-05-14 20:46:12 +0200

    init

---
+++ ex02/include/Array.class.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.class.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfuhlenb <vfuhlenb@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/14 20:02:52 by vfuhlenb          #+#    #+#             */
+/*   Updated: 2023/05/14 20:34:05 by vfuhlenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_CLASS_HPP
+#define ARRAY_CLASS_HPP
+
+template< typename T >
+class Array
+{
+	public:
+		Array();
+		~Array();
+};
+
+#include "Array.class.tpp"
+
+#endif
---
+++ ex02/include/Array.class.tpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.class.tpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfuhlenb <vfuhlenb@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/14 20:02:50 by vfuhlenb          #+#    #+#             */
+/*   Updated: 2023/05/14 20:34:42 by vfuhlenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_CLASS_TPP
+#define ARRAY_CLASS_TPP
+
+template<typename T>
+Array<T>::Array() {}
+
+tempöate<typename T>
+Array<T>::~Array() {}
+
+
+#endif
