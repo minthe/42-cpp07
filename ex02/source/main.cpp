dafbdbb26aef6bdd512539ea2698e49d871b8f99
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2023-05-15 14:50:42 +0200

    expanded test1

--- ex02/source/main.cpp
+++ ex02/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 19:55:32 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/05/15 14:35:22 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2023/05/15 14:49:28 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@
 int	main()
 {
 	{	
-		try
-		{
+		try {
 			std::cout << "\x1b[31mTest 1\x1b[0m\n"<< std::endl;
 
 			Array<float> test(25);
 			Array<int> test2(15);
 			Array<int> test3;
+			Array<int> test4;
 			std::cout << "test1: " << test.size() << std::endl;
 			std::cout << "test2: " << test2.size() << std::endl;
 			std::cout << "test3: " << test3.size() << std::endl;
@@ -32,10 +32,12 @@ int	main()
 			std::cout << "test3: " << test3.size() << "_element[" << TestInd << "] '" << test3[TestInd] << "'" << std::endl; // access element with subscript overload
 			test3[-4] = 99;
 			std::cout << "test3: " << test3.size() << "_element[" << TestInd << "] '" << test3[TestInd] << "'" << std::endl; // access element with subscript overload
+			test4[0] = 1;
+			std::cout << "test4: " << test4.size() << "_element[" << 0 << "] '" << test4[0] << "'" << std::endl; // access element with subscript overload
 		}
-		catch (const std::exception& e)
-		{
-			std::cerr << "exception caught: " << e.what() << '\n';
+
+		catch (const std::exception& e) {
+			std::cerr << "\x1b[31mexception caught: " << e.what() << "\x1b[0m" << std::endl;
 		}
 	
 	}
