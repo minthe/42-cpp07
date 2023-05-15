1f560f5f03e0ddf74d513a33025e8c9bdc79102c
Valentin Fuhlenbrok <vfuhlenb@student.42wolfsburg.de>
2023-05-15 13:58:56 +0000

    added test2 with consts, need to work on const input

--- ex02/source/main.cpp
+++ ex02/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 19:55:32 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/05/15 13:40:04 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2023/05/15 13:56:56 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@
 
 int	main()
 {
+	
+// Test 1:
 	{	
 		try {
-			std::cout << "\x1b[31mTest 1\x1b[0m\n"<< std::endl;
+			std::cout << "\x1b[47mTest 1\x1b[0m\n"<< std::endl;
 
 			Array<int> test(25);
 			Array<int> test2;
@@ -29,20 +31,36 @@ int	main()
 			std::cout << "test2: " << test2.size() << std::endl;
 			std::cout << "test3: " << test3.size() << std::endl;
 			test3 = test;
-			std::cout << "test3: " << test3.size() << "_element[" << TestInd << "] '" << test3[TestInd] << "'" << std::endl; // access element with subscript overload
+			std::cout << "test3: " << test3.size() << "_element[" << TestInd << "] '" << test3[TestInd] << "'" << std::endl;
 			test3[10] = 99;
-			std::cout << "test3: " << test3.size() << "_element[" << TestInd << "] '" << test3[TestInd] << "'" << std::endl; // access element with subscript overload
+			std::cout << "test3: " << test3.size() << "_element[" << TestInd << "] '" << test3[TestInd] << "'" << std::endl;
 			test3 = test;
-			std::cout << "test3: " << test3.size() << "_element[" << TestInd << "] '" << test3[TestInd] << "'" << std::endl; // access element with subscript overload
-			std::cout << "test: " << test.size() << "_element[" << TestInd << "] '" << test[TestInd] << "'" << std::endl; // access element with subscript overload
+			std::cout << "test3: " << test3.size() << "_element[" << TestInd << "] '" << test3[TestInd] << "'" << std::endl;
+			std::cout << "test: " << test.size() << "_element[" << TestInd << "] '" << test[TestInd] << "'" << std::endl;
 			// test4[0] = 1;
-			// std::cout << "test4: " << test4.size() << "_element[" << 0 << "] '" << test4[0] << "'" << std::endl; // access element with subscript overload
+			// std::cout << "test4: " << test4.size() << "_element[" << 0 << "] '" << test4[0] << "'" << std::endl;
 		}
 
 		catch (const std::exception& e) {
 			std::cerr << "\x1b[31mexception caught: " << e.what() << "\x1b[0m" << std::endl;
 		}
-	
+	}
+
+// Test 2: const instances
+	{	
+		try {
+			std::cout << "\x1b[47m\nTest 2: const instances\x1b[0m\n"<< std::endl;
+
+			const Array<int> test(25);
+			std::cout << "test: " << test.size() << "_element[" << TestInd << "] '" << test[TestInd] << "'" << std::endl;
+			test[10] = 2;
+			std::cout << "test: " << test.size() << "_element[" << TestInd << "] '" << test[TestInd] << "'" << std::endl;
+		}
+
+		catch (const std::exception& e) {
+			std::cerr << "\x1b[31mexception caught: " << e.what() << "\x1b[0m" << std::endl;
+		}
 	}
 	return 0;
+
 }
